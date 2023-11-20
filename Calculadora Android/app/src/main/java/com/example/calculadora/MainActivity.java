02b8ccf8875b70592c3e983b2beebbba5cf118d1
Titi679 <titicarles@gmail.com>
2023-11-20 11:39:46 +0100

    Calculadora terminada

--- Calculadora Android/app/src/main/java/com/example/calculadora/MainActivity.java
+++ Calculadora Android/app/src/main/java/com/example/calculadora/MainActivity.java
@@ -1,106 +1,139 @@
 package com.example.calculadora;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 import android.widget.Switch;
-import java.text.DecimalFormat;
-import java.util.Locale;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    private String operadorActual = "";
-    private double numero1 = 0.0;
-    private boolean nuevoNumero = true;
-    private boolean enRadianes = true;
-    TextView textView3;
+    private TextView resultTextView;
+    private StringBuilder input = new StringBuilder();
+    private boolean isRadians = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textView3 = (TextView) findViewById(R.id.textView3);
-        Switch switchModo = findViewById(R.id.switch1);
-        switchModo.setOnCheckedChangeListener((buttonView, isChecked) -> {
-            // Cambiar entre grados y radianes cuando cambia el estado del switch
-            enRadianes = isChecked;
+        resultTextView = findViewById(R.id.textView3);
+
+        Switch switchDegreesRadians = findViewById(R.id.switch1);
+        switchDegreesRadians.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            isRadians = isChecked;
+            updateResult();
         });
     }
+
     public void onClickNumero(View view) {
-        if (nuevoNumero) {
-            textView3.setText("");
-            nuevoNumero = false;
-        }
-        Button boton = (Button) view;
-        String numero = boton.getText().toString();
-        String textoActual = textView3.getText().toString();
-        textView3.setText(textoActual + numero);
+        Button button = (Button) view;
+        input.append(button.getText());
+        updateResult();
     }
 
     public void onClickOperador(View view) {
-        Button boton = (Button) view;
-        String operador = boton.getText().toString();
-
-        if (!operadorActual.isEmpty()) {
-            return; // Evitar que el usuario ingrese múltiples operadores consecutivos
-        }
-
-        operadorActual = operador;
-        numero1 = Double.parseDouble(textView3.getText().toString());
-        nuevoNumero = true;
+        Button button = (Button) view;
+        input.append(" ").append(button.getText()).append(" ");
+        updateResult();
     }
 
     public void onClick_res(View view) {
-        if (operadorActual.isEmpty()) {
-            return; // No se ha seleccionado un operador
+        try {
+            String expression = input.toString();
+            // Realiza el cálculo de la expresión
+            double result = evaluarExpresion(expression);
+            // Muestra el resultado
+            resultTextView.setText(String.valueOf(result));
+            // Limpia la entrada
+            input.setLength(0);
+        } catch (Exception e) {
+            resultTextView.setText("Error");
         }
-
-        double numero2 = Double.parseDouble(textView3.getText().toString());
-        double resultado = 0.0;
-
-        switch (operadorActual) {
-            case "+":
-                resultado = numero1 + numero2;
-                break;
-            case "-":
-                resultado = numero1 - numero2;
-                break;
-            case "*":
-                resultado = numero1 * numero2;
-                break;
-            case "/":
-                if (numero2 != 0) {
-                    resultado = numero1 / numero2;
-                } else {
-                    textView3.setText("Error");
-                    return;
-                }
-                break;
-        }
-
-        // Aplicar seno, coseno o tangente si corresponde
-        if (view.getId() == R.id.button_Sin) {
-            resultado = Math.sin(enRadianes ? Math.toRadians(resultado) : resultado);
-        } else if (view.getId() == R.id.button_Cos) {
-            resultado = Math.cos(enRadianes ? Math.toRadians(resultado) : resultado);
-        } else if (view.getId() == R.id.button_Tan) {
-            resultado = Math.tan(enRadianes ? Math.toRadians(resultado) : resultado);
-        }
-
-        // Formatear el resultado con dos decimales
-        DecimalFormat formato = new DecimalFormat("#.##");
-        String resultadoFormateado = formato.format(resultado);
-
-        textView3.setText(resultadoFormateado);
-        operadorActual = "";
-        nuevoNumero = true;
     }
-    public void onClick_AC (View view) {
-        textView3.setText("");
-        operadorActual = "";
-        nuevoNumero = true;
+
+    public void onClick_Sin(View view) {
+        try {
+            double value = Double.parseDouble(resultTextView.getText().toString());
+            double result;
+            if (isRadians) {
+                result = Math.sin(value);
+            } else {
+                result = Math.sin(Math.toRadians(value));
+            }
+            resultTextView.setText(String.valueOf(result));
+        } catch (NumberFormatException e) {
+            resultTextView.setText("Error");
+        }
+    }
+
+    public void onClick_Cos(View view) {
+        try {
+            double value = Double.parseDouble(resultTextView.getText().toString());
+            double result;
+            if (isRadians) {
+                result = Math.cos(value);
+            } else {
+                result = Math.cos(Math.toRadians(value));
+            }
+            resultTextView.setText(String.valueOf(result));
+        } catch (NumberFormatException e) {
+            resultTextView.setText("Error");
+        }
+    }
+
+    public void onClick_Tan(View view) {
+        try {
+            double value = Double.parseDouble(resultTextView.getText().toString());
+            double result;
+            if (isRadians) {
+                result = Math.tan(value);
+            } else {
+                result = Math.tan(Math.toRadians(value));
+            }
+            resultTextView.setText(String.valueOf(result));
+        } catch (NumberFormatException e) {
+            resultTextView.setText("Error");
+        }
+    }
+
+    public void onClick_AC(View view) {
+        input.setLength(0);
+        resultTextView.setText("");
+    }
+
+    private void updateResult() {
+        resultTextView.setText(input.toString());
+    }
+
+    private double evaluarExpresion(String expression) {
+        // Puedes implementar aquí la lógica de evaluación de expresiones matemáticas
+        // Puedes usar la clase ScriptEngine o evaluar manualmente la expresión.
+        // Aquí hay un ejemplo simple usando evaluar manualmente la expresión:
+        String[] tokens = expression.split(" ");
+        double operand1 = Double.parseDouble(tokens[0]);
+        double operand2 = Double.parseDouble(tokens[2]);
+        String operator = tokens[1];
+
+        switch (operator) {
+            case "+":
+                return operand1 + operand2;
+            case "-":
+                return operand1 - operand2;
+            case "*":
+                return operand1 * operand2;
+            case "/":
+                if (operand2 != 0) {
+                    return operand1 / operand2;
+                } else {
+                    throw new ArithmeticException("División por cero");
+                }
+            default:
+                throw new IllegalArgumentException("Operador no válido");
+        }
     }
 }
+
+
